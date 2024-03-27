dfa5a63c9d1c8e506e421e89f6cc579775d6d1ae
Max S. Haberman <max@surveycto.com>
2024-03-27 18:01:18 -0400

    Removed test and unused code

--- source/basic_decryption.py
+++ source/basic_decryption.py
@@ -1,21 +1,15 @@
 from scto_encryption.crypto import Encryption, CryptoKey, Encrypted
-from base64 import urlsafe_b64decode, b64encode, urlsafe_b64encode, b64decode
+from base64 import b64encode, b64decode
 from time import time
 
-import hmac
-import hashlib
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.hmac import HMAC
-
-def genHmac1(string, signing_key):
-  return hmac.new(signing_key, msg=string.decode('utf-8'), digestmod=hashlib.sha256).digest()
 
 def genHmac(data, signing_key):
   h = HMAC(b64decode(signing_key), hashes.SHA256())
   h.update(data)
   hmac = h.finalize()
   return hmac
-
 
 def basic():
   key = CryptoKey.fromKey(b'haGwM/krSlYJ7UZG2FGPfkUJh2Pr0OVEDnrn2WUB2YY=')
@@ -24,7 +18,6 @@ def basic():
   encryption_token = b'gAAAAABmBIANsAkgqKFdhPgOmb3P5INIEuGMUgdirvcU4Y2J5AIY8/zfuL+nXHxtJxC+GsqOlctR0HDrXOcRv4PViqbvBOfOIQ=='
 
   encrypted = Encrypted.fromToken(encryption_token)
-
   decrypted = encryption.decrypt(encrypted)
 
   print(encrypted.ciphertext)
@@ -35,51 +28,27 @@ def basic():
 def main():
   key = CryptoKey.fromKey(b'haGwM/krSlYJ7UZG2FGPfkUJh2Pr0OVEDnrn2WUB2YY=')
 
-  encryption1 = Encryption(key)
-  encrypted1 = encryption1.encrypt(b'You found me!')
-
-  # ciphertext = b'4YxSB2Ku9xThjYnkAhjz/A=='
-  # iv = b'sAkgqKFdhPgOmb3P5INIEg=='
-  ciphertext = encrypted1.ciphertext
-  iv = encrypted1.iv
+  ciphertext = b'4YxSB2Ku9xThjYnkAhjz/A=='
+  iv = b'sAkgqKFdhPgOmb3P5INIEg=='
 
   decoded_ct = b64decode(ciphertext)
   decoded_iv = b64decode(iv)
 
-  version = b'\x80'
-  # timestamp = int(time()).to_bytes(8, 'big')
-  timestamp = b64decode(encrypted1.timestamp)
-  hmac_raw = version + timestamp + decoded_iv + decoded_ct
+  timestamp = int(time()).to_bytes(8, 'big')
+  hmac_data = b'\x80' + timestamp + decoded_iv + decoded_ct
 
+  hmac = genHmac(hmac_data, key.signing_key)
+  constructed_token_bytes = hmac_data + hmac
+  constructed_token = b64encode(constructed_token_bytes)
 
-
-  print('HMAC parts 2')
-  print(timestamp)
-  print(decoded_iv)
-  print(decoded_ct)
-  print(hmac_raw)
-  print(key.signing_key)
-
-  hmac = genHmac(hmac_raw, key.signing_key)
-  print(len(hmac))
-  print(hmac)
-  constructed_token_bytes = hmac_raw + hmac
-  constructed_token = urlsafe_b64encode(constructed_token_bytes)
-  print(constructed_token)
-
-  encrypted2 = Encrypted.fromToken(constructed_token)
+  encrypted = Encrypted.fromToken(constructed_token)
   
-  # encrypted2 = 
-
   encryption = Encryption(key)
-  print(encrypted1.token)
-  print(encrypted2.token)
-  decrypted1 = encryption.decrypt(encrypted1)
-  decrypted2 = encryption.decrypt(encrypted2)
-  print(decrypted2)
-
-  # constructed token - 
-  pass
+  print('Full encryption token:')
+  print(encrypted.token)
+  decrypted = encryption.decrypt(encrypted)
+  print('Decrypted message:')
+  print(decrypted)
 
 
 if __name__ == '__main__':

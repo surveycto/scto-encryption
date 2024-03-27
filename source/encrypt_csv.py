34eedcdebac66e86df9eedc3d0efc4df3036e7a3
Max S. Haberman <max@surveycto.com>
2024-03-27 16:14:45 -0400

    Updated path to sample data files

--- source/encrypt_csv.py
+++ source/encrypt_csv.py
@@ -1,11 +1,15 @@
 from scto_encryption.crypto import CryptoKey
 from scto_encryption import csv_encrypt
 
+from os.path import dirname, join
+
 def main():
   # key = CryptoKey.generate()
+  repo_path = dirname(dirname(__file__))
   key = CryptoKey.fromKey(b'haGwM/krSlYJ7UZG2FGPfkUJh2Pr0OVEDnrn2WUB2YY=')
-  original_path = '/Users/max.s.haberman/Documents/SurveyCTO code/Field plug-ins by Max/In progress/decrypt/extras/sample-form/data.csv'
-  new_path = '/Users/max.s.haberman/Documents/SurveyCTO code/Field plug-ins by Max/In progress/decrypt/extras/sample-form/encrypted_data.csv'
+  data_path = join(repo_path, 'sample_data')
+  original_path = join(data_path, 'data.csv')
+  new_path = join(data_path, 'encrypted_data.csv')
   csv_encrypt.encryptCsv(key, original_path, new_path, exclude_headers = ['id_key'])
   encryption_key = key.encryption_key.decode('utf-8')
   print(f'Your decryption key:\n{encryption_key}')

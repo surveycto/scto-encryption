c733b68983d352940f46796f636c2f04b139bfbc
Max S. Haberman <max@surveycto.com>
2024-03-27 18:53:47 -0400

    Added decryption tools

--- source/basic_decryption.py
+++ source/basic_decryption.py
@@ -1,55 +1,14 @@
 from scto_encryption.crypto import Encryption, CryptoKey, Encrypted
-from base64 import b64encode, b64decode
-from time import time
-
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.hmac import HMAC
-
-def genHmac(data, signing_key):
-  h = HMAC(b64decode(signing_key), hashes.SHA256())
-  h.update(data)
-  hmac = h.finalize()
-  return hmac
-
-def basic():
-  key = CryptoKey.fromKey(b'haGwM/krSlYJ7UZG2FGPfkUJh2Pr0OVEDnrn2WUB2YY=')
-  encryption = Encryption(key)
-
-  encryption_token = b'gAAAAABmBIANsAkgqKFdhPgOmb3P5INIEuGMUgdirvcU4Y2J5AIY8/zfuL+nXHxtJxC+GsqOlctR0HDrXOcRv4PViqbvBOfOIQ=='
-
-  encrypted = Encrypted.fromToken(encryption_token)
-  decrypted = encryption.decrypt(encrypted)
-
-  print(encrypted.ciphertext)
-  print(encrypted.iv)
-
-  print(f'Decrypted:\n{decrypted}')
 
 def main():
-  key = CryptoKey.fromKey(b'haGwM/krSlYJ7UZG2FGPfkUJh2Pr0OVEDnrn2WUB2YY=')
-
+  encryption_key = b'RQmHY+vQ5UQOeufZZQHZhg=='
   ciphertext = b'4YxSB2Ku9xThjYnkAhjz/A=='
   iv = b'sAkgqKFdhPgOmb3P5INIEg=='
 
-  decoded_ct = b64decode(ciphertext)
-  decoded_iv = b64decode(iv)
-
-  timestamp = int(time()).to_bytes(8, 'big')
-  hmac_data = b'\x80' + timestamp + decoded_iv + decoded_ct
-
-  hmac = genHmac(hmac_data, key.signing_key)
-  constructed_token_bytes = hmac_data + hmac
-  constructed_token = b64encode(constructed_token_bytes)
-
-  encrypted = Encrypted.fromToken(constructed_token)
-  
+  key = CryptoKey.fromEncryptionKey(encryption_key)
+  encrypted = Encrypted.tokenless(iv, ciphertext, key)
   encryption = Encryption(key)
-  print('Full encryption token:')
-  print(encrypted.token)
-  decrypted = encryption.decrypt(encrypted)
-  print('Decrypted message:')
-  print(decrypted)
-
-
+  print(encryption.decrypt(encrypted))
+  
 if __name__ == '__main__':
   main()
--- source/scto_encryption/crypto.py
+++ source/scto_encryption/crypto.py
@@ -1,11 +1,23 @@
-from base64 import urlsafe_b64decode, b64encode
+from base64 import urlsafe_b64decode, b64encode, b64decode
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.hmac import HMAC
+from time import time
+from os import urandom
+
+from typing import Union
 
 def base64urlToStandard(data: bytes) -> bytes:
   return data.replace(b'-', b'+').replace(b'_', b'/')
 
 def base64standardToUrl(data: bytes) -> bytes:
   return data.replace(b'+', b'-').replace(b'/', b'_')
+
+def strToBytes(data: Union[str, bytes]) -> bytes:
+  if type(data) == str:
+    return data.encode('utf-8')
+  else:
+    return data
 
 class CryptoKey:
   def __init__(
@@ -21,12 +33,18 @@ class CryptoKey:
   @staticmethod
   def fromKey(key: bytes) -> 'CryptoKey':
     decoded_key = urlsafe_b64decode(key)
-    signing_key_raw = decoded_key[0:16]
-    encryption_key_raw = decoded_key[16:]
-    signing_key = b64encode(signing_key_raw)
-    encryption_key = b64encode(encryption_key_raw)
+    decoded_signing_key = decoded_key[0:16]
+    decoded_encryption_key = decoded_key[16:]
+    signing_key = b64encode(decoded_signing_key)
+    encryption_key = b64encode(decoded_encryption_key)
     return CryptoKey(base64urlToStandard(key), signing_key, encryption_key)
   
+  @staticmethod
+  def fromEncryptionKey(encryption_key: bytes) -> 'CryptoKey':
+    decoded_signing_key = urandom(16)
+    whole_key = b64encode(decoded_signing_key + b64decode(encryption_key))
+    return CryptoKey(whole_key, b64encode(decoded_signing_key), encryption_key)
+
   @staticmethod
   def generate() -> "CryptoKey":
     key = Fernet.generate_key()
@@ -48,6 +66,13 @@ class Encrypted:
     self.iv = iv
     self.ciphertext = ciphertext
     self.hmac = hmac
+  
+  # Should be decoded byte data, NOT Base64.
+  @staticmethod
+  def _genHmac(data: bytes, signing_key: bytes) -> bytes:
+    h = HMAC(signing_key, hashes.SHA256())
+    h.update(data)
+    return h.finalize()
 
   @staticmethod
   def fromToken(token: bytes) -> "Encrypted":
@@ -58,6 +83,27 @@ class Encrypted:
     ciphertext = b64encode(decoded_token[25:len(decoded_token) - 32])
     hmac = b64encode(decoded_token[-32:])
     return Encrypted(base64urlToStandard(token), version, timestamp, iv, ciphertext, hmac)
+  
+  @staticmethod
+  def tokenless(
+    iv: Union[bytes, str],
+    ciphertext: Union[bytes, str],
+    key: CryptoKey) -> 'Encrypted':
+    iv = strToBytes(iv)
+    ciphertext = strToBytes(ciphertext)
+
+    signing_key = key.signing_key
+    decoded_signing_key = b64decode(signing_key)
+    decoded_ct = b64decode(ciphertext)
+    decoded_iv = b64decode(iv)
+
+    timestamp = int(time()).to_bytes(8, 'big')
+    hmac_data = b'\x80' + timestamp + decoded_iv + decoded_ct
+    hmac = Encrypted._genHmac(hmac_data, decoded_signing_key)
+    decoded_constructed_token = hmac_data + hmac
+    constructed_token = b64encode(decoded_constructed_token)
+    return Encrypted.fromToken(constructed_token)
+
 
 class Encryption:
   def __init__(self, key: CryptoKey):

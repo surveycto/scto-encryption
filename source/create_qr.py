7193ad920fd607a83cadcabf946d90dd7a178249
Max S. Haberman <max@surveycto.com>
2024-04-10 20:14:04 -0400

    Added create folder if it does not exist

--- source/create_qr.py
+++ source/create_qr.py
@@ -1,11 +1,16 @@
 from scto_encryption.functions import generateKey
 
 import qrcode
-from os.path import dirname, join
+from os.path import dirname, join, exists
+from os import mkdir
 
 def main():
   repo_path = dirname(dirname(__file__))
   data_path = join(repo_path, 'sample_data')
+
+  if not exists(data_path):
+    mkdir(data_path)
+
   key_path = join(data_path, 'sample_key_for_qr.pem')
   encryption_key = generateKey(key_path)
   print('Encryption key:')
